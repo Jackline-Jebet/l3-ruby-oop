57fc0b8f99c0853280948e2f22a7949fb14274b4
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-18 13:20:33 +0300

    completed practice

--- animal.rb
+++ animal.rb
@@ -1,3 +1,44 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
+class Animal
+    
+    attr_reader :name
+    attr_writer :species
+    attr_accessor :life_expectancy
+    @species
+    @life_expectancy
+
+    #getter for the name of the Animal
+    def name
+        @name
+    end
+
+    #setter for the name of the Animal
+    def name=(name)
+        @name = name
+    end
+
+    def sound
+        puts "#{self.name} is sounding"
+    end
+end
+
+dog = Animal.new
+dog.name = "Simba"
+dog.sound
+dog.species = "Terrier"
+dog.life_expectancy = 13
+
+
+cat = Animal.new
+cat.name = "Atta"
+puts cat
+cat.sound
+
+lioness = Animal.new
+#create name of the Animal
+lioness.name = "Nala"
+#retreive name of the Animal
+puts lioness.name
+lioness.sound
 
 # TODO: Use self to access instance variables
--- plant.rb
+++ plant.rb
@@ -1,1 +1,49 @@
 # TODO: Implement a class do illustrate how class variables and methods work.
+class Plant
+
+    @@num_of_trees = 0
+    @@all_instances = []
+
+    attr_accessor :species
+
+    def initialize(species)
+        @species = species
+        @@num_of_trees += 1
+        info = {
+            instance_reference: self,
+            number: @@num_of_trees
+        }
+        @@all_instances << info
+    end
+
+    def speak_of_species
+        puts "#{self.species} is speaking"
+    end
+
+    def self.tree_count
+        @@num_of_trees
+    end
+
+    def self.show_instances
+        @@all_instances
+    end
+
+end
+
+#ACCESs CLASS VARIABLE
+mwarubaini = Plant.new("Neem Tree")
+mwarubaini.speak_of_species
+puts Plant.tree_count
+
+mango = Plant.new("Mango Tree")
+orange = Plant.new("Orange Tree")
+cypress = Plant.new("Cypress Tree")
+eucalyptus = Plant.new("Eucalyptus Tree")
+
+# puts mango.tree_count
+# puts orange.tree_count
+# puts cypress.tree_count
+# puts eucalyptus.tree_count
+
+puts Plant.tree_count
+puts Plant.show_instances
